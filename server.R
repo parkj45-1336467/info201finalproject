6f81b58f0e4488769d6b58cff578a0707e5ec68e
pkelaita <pierce@kelaita.com>
2018-03-03 17:33:14 -0800

    added panel buttons

--- server.R
+++ server.R
@@ -1,5 +1,24 @@
+library(dplyr)
+
 nba <- read.csv("data/nba_season_data.csv", stringsAsFactors=FALSE)
+nba <- nba %>% filter(height > 0)
 
 server <- function(input, output) {
-  
+    output$heightPlot <- renderPlot({
+     plot(nba$height, nba[, input$stat])
+  })
+   
+   # scatter.x <- nba %>% select(height)
+   # scatter.y <- reactive({
+   #   nba %>% select(input$stat)
+   # })
+   # 
+   # output$plot1 <- renderPlot({
+   #   ggplot(nba) +
+   #     geom_point(mapping = aes(x = Scatter.x, y = Scatter.y()), color = "red", size = 4) +
+   #     labs(title = paste("Height vs", input$stat)
+   #     )
+   # })
 }
+
+shinyServer(server)
--- ui.R
+++ ui.R
@@ -5,16 +5,25 @@ ui <- fluidPage(
   titlePanel("NBA Data!"),
   sidebarLayout(
     sidebarPanel(
-      sliderInput(inputId = "slider",
+      sliderInput(inputId = "year",
                   label = "Year playing",
                   min = min(nba$year),
                   max = max(nba$year),
                   step = 1,
                   value = c(2000, 2016),
-                  sep = "")
+                  sep = ""),
+      radioButtons(
+        inputId="stat",
+        label="Stat",
+        choices=c(
+          "ORB"="orb",
+          "DRB"="drb",
+          "TRB"="trb"
+        )
+      )
     ),
     mainPanel(
-      plotOutput(outputId = "distPlot")
+      plotOutput(outputId = "heightPlot")
     )
   )
 )
