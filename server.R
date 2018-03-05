18e084ada77c121d9a67149ea341b51eee818e27
pkelaita <pierce@kelaita.com>
2018-03-05 12:18:32 -0800

    improved code style and added comments

--- server.R
+++ server.R
@@ -2,52 +2,57 @@ library(dplyr)
 library(ggplot2)
 library(shiny)
 
+### Data Wrangling
 
+# our initial dataset from the CSV file
 nba <- read.csv("./data/nba_season_data.csv", stringsAsFactors=FALSE)
+
+# The NBA reorganized its teams in 2004, so we need this dataset for
+# functions that organize statistics by team
 nba2004 <- nba %>% filter(year >= 2004 & truesalary != "" & height > 0) %>%
   mutate(height.feet = height * 0.0833333)
+
+# For our functions that do not organize by team, we can include all the teams from
+# 1978 - 2016 (the year range provided in the CSV file)
 modifieddata <- nba %>% filter(height > 0) %>% mutate(heightinft = height*0.083 )
 heightsdata <- modifieddata %>% group_by(year) %>% summarize(mean = mean(heightinft))
 
-
-
+### Creating Pages 
 server <- function(input, output){
-  specData <- eventReactive(input$update,{
-    teamData <- nba2004 %>% filter(tm == input$team & year == input$year)
-    teamData$truesalary <- as.numeric(gsub("[\\$,]","",teamData$truesalary))
-    teamData <- arrange(teamData, truesalary)
-  }, ignoreNULL = FALSE)
   
-  gradient <- c("turquoise2", "violet", "navy")
+  ### --------------- Page 1: Height Over Time --------------- ###
   
-  output$plot <- renderPlot({
-    validate(need(nrow(specData()) > 0,
-                  "No data is availible for this query."))
-    validate(need(input$year >= 2004 & input$year <= 2016,
-                  "Please choose a year between 2004 and 2016!"))
-    ggplot(data = specData()) +
-      geom_bar(mapping = aes(x = reorder(player, height.feet),
-                             y = truesalary / 1e+6,
-                             fill = height.feet),
-               stat = "identity") +
-      labs(x = "Player Name",
-           y = "Salary (Dollars)")+
-      coord_flip() +
-      scale_fill_gradientn(colors = gradient, 
-                           guide = guide_colorbar(title = "Height (Feet)")) +
-      scale_y_continuous(name = "Salary (US Dollars)",
-                         labels = scales::dollar_format(
-                           prefix = "$", suffix = " million",
-                           largest_with_cents = 1e+5,
-                           big.mark = "",
-                           negative_parens = FALSE)) +
-      theme(text = element_text(size=17))
-  },
-  height = 500, width = 800)
+  output$plot.p1 <- renderPlot({
+    yearseq <- seq(input$Years[1], input$Years[2])
+    x <- heightsdata[which(heightsdata$year %in% yearseq),]
+    ggplot(data = x, aes(x = year, y = mean)) + 
+      geom_point(shape = 22,
+                 color = "black",
+                 fill = "white",
+                 size = 4,
+                 stroke = 4) +
+      xlab("Range of Years") +
+      ylab("Range of Heights (in feet)") +
+      geom_smooth(method = lm, se= TRUE, color = "darkred", linetype= "dotted") +
+      ylim(c(6.45,6.6)) +
+      ggtitle("Height vs. Year") + 
+      theme(plot.title = element_text(size = "20", hjust = "0.5", face= "bold"),
+            axis.text.x = element_text( size = "12"),
+            axis.text.y = element_text( size = "12"),
+            axis.title.x = element_text(size = "13"),
+            axis.title.y = element_text(size = "13"),
+            panel.background = element_rect(fill= "lightblue" , color = "blue"))
+    
+  })
   
-  output$statsGraph <- renderPlot({
-    nba <- modifieddata %>% filter (year >= input$years[1] & year <= input$years[2] & per > 0 & orb > 0 & drb > 0 & trb > 0 &
-                             blk > 0 & stl >0 & tov > 0) %>% select(per, orb, drb, trb, blk, stl, tov, heightinft)
+  ### --------------- Page 2: Height Versus Game Stats --------------- ###
+  
+  output$plot.p2 <- renderPlot({
+    nba <- modifieddata %>% filter (year >= input$years[1] &
+                                      year <= input$years[2] &
+                                      per > 0 & orb > 0 & drb > 0 & trb > 0 &
+                                      blk > 0 & stl >0 & tov > 0) %>%
+      select(per, orb, drb, trb, blk, stl, tov, heightinft)
     
     if (input$stats == "Efficiency") {
       stats <- nba %>% arrange(heightinft) %>% group_by(heightinft) %>% summarise(mean = mean(per))
@@ -101,27 +106,45 @@ server <- function(input, output){
     }
   })
   
-  output$heightvsyearPlot <- renderPlot({
-    yearseq <- seq(input$Years[1], input$Years[2])
-    x <- heightsdata[which(heightsdata$year %in% yearseq),]
-    ggplot(data = x, aes(x = year, y = mean)) + 
-      geom_point(shape = 22,
-                 color = "black",
-                 fill = "white",
-                 size = 4,
-                 stroke = 4) +
-      xlab("Range of Years") +
-      ylab("Range of Heights (in feet)") +
-      geom_smooth(method = lm, se= TRUE, color = "darkred", linetype= "dotted") +
-      ylim(c(6.45,6.6)) +
-      ggtitle("Height vs. Year") + 
-      theme(plot.title = element_text(size = "20", hjust = "0.5", face= "bold"),
-            axis.text.x = element_text( size = "12"),
-            axis.text.y = element_text( size = "12"),
-            axis.title.x = element_text(size = "13"),
-            axis.title.y = element_text(size = "13"),
-            panel.background = element_rect(fill= "lightblue" , color = "blue"))
+  ### --------------- Page 3: Height Versus Salary by Team --------------- ###
   
- }) }
+  output$plot.p3 <- renderPlot({
+    
+    # Reactive function to read data from users
+    specData <- eventReactive(input$update,{
+      teamData <- nba2004 %>% filter(tm == input$team & year == input$year)
+      teamData$truesalary <- as.numeric(gsub("[\\$,]","",teamData$truesalary))
+      teamData <- arrange(teamData, truesalary)
+    }, ignoreNULL = FALSE)
+    
+    # Handle exceptions with user input
+    validate(need(nrow(specData()) > 0,
+                  "No data is availible for this query."))
+    validate(need(input$year >= 2004 & input$year <= 2016,
+                  "Please choose a year between 2004 and 2016!"))
+    
+    # Create the plot
+    ggplot(data = specData()) +
+      geom_bar(mapping = aes(x = reorder(player, height.feet),
+                             y = truesalary / 1e+6,
+                             fill = height.feet),
+               stat = "identity") +
+      labs(x = "Player Name",
+           y = "Salary (Dollars)")+
+      coord_flip() +
+      scale_fill_gradientn(colors = c("turquoise2", "violet", "navy"), 
+                           guide = guide_colorbar(title = "Height (Feet)")) +
+      scale_y_continuous(name = "Salary (US Dollars)",
+                         labels = scales::dollar_format(
+                           prefix = "$", suffix = " million",
+                           largest_with_cents = 1e+5,
+                           big.mark = "",
+                           negative_parens = FALSE)) +
+      theme(text = element_text(size=17)) +
+      ggtitle(paste0(input$team, " Player Salaries for the ", input$year, " Season"))
+  },
+  height = 500, width = 800)
+  
+  }
 
 shinyServer(server)
--- ui.R
+++ ui.R
@@ -4,64 +4,78 @@ library(shiny)
 library(shinythemes)
 
 nba2004 <- nba %>% filter(year >= 2004 & truesalary != "")
-# Define UI for app that draws a histogram ----
-  ui <- navbarPage(theme = shinytheme("cerulean"), "NBA vs. Height",
-                   tabPanel("Home",
-                   sidebarLayout(
-                     sidebarPanel(tags$img(src="nbalogo.jpg", height =250 , width =250),
-                       sliderInput("Years",
-                                   "Range of Years",
-                                   min = min(heightsdata$year),
-                                   max = max(heightsdata$year),
-                                   step = 1 ,
-                                   value = c(1978, 2016),
-                                   sep = "")
-                       , tags$img(src="shaq.jpg", height =250 , width =250)
-                       
-                       
-                
-                       
-                     ),
-                     mainPanel(
-                       plotOutput("heightvsyearPlot")
-                     )
-                    )
-                   ),
-                   
-    tabPanel("Height vs. Stats",
-      sidebarLayout(
-      sidebarPanel(
-        hr(),
-        sliderInput("years", "Years:",
-                    min = 2004, max = 2016,
-                    value = c(2004,2016)),
-        br(),
-        radioButtons("stats", h4("Stats"),
-                      c("Efficiency", "Defensive Rebound", "Offensive Rebound", "Rebound", "Block", "Steal", "Turnover")),
-        hr()
-      ),
-      mainPanel(
-        plotOutput("statsGraph")
-        )
-      )
-    ),               
-                                      
+
+# Define UI for app that has multiple tabs
+
+ui <- navbarPage( theme = shinytheme("cerulean"), "NBA Interactive Height Statistics",
+
+    ## Page 1: Height Over Time
+    tabPanel("Height Over Time",
+             sidebarLayout(
+               sidebarPanel(
+                 tags$img(src = "nbalogo.jpg", height = 150),
+                 sliderInput(
+                   "Years",
+                   "Range of Years",
+                   min = min(heightsdata$year),
+                   max = max(heightsdata$year),
+                   step = 1 ,
+                   value = c(1978, 2016),
+                   sep = ""),
+                 tags$img(src = "shaq.jpg", height = 300)
+               ),
+               mainPanel(plotOutput("plot.p1"))
+             )),
+    
+    ## Page 2: Height vs. Game Stats
+    tabPanel("Height vs. Game Stats",
+             sidebarLayout(
+               sidebarPanel(
+                 hr(),
+                 sliderInput(
+                   "years",
+                   "Years:",
+                   min = 2004,
+                   max = 2016,
+                   value = c(2004, 2016)
+                 ),
+                 br(),
+                 radioButtons("stats",
+                   h4("Game Statistic"),
+                   c(
+                     "Efficiency",
+                     "Defensive Rebound %",
+                     "Offensive Rebound %",
+                     "Rebounds",
+                     "Blocks",
+                     "Steals",
+                     "Turnovers"
+                   )
+                 ),
+                 hr()
+               ),
+               mainPanel(plotOutput("plot.p2"))
+             )),
+    
+    ## Page 3: Height vs. Salary
     tabPanel("Height vs. Salary",
-    sidebarLayout(
-      sidebarPanel(
-        selectInput("team", "Select a team:", nba2004 %>%
-                      distinct(tm, .keep_all = TRUE) %>%
-                      rename(Team=tm) %>%
-                      select(Team),
-                    selected = "GSW"),
-        numericInput("year", "Enter a year:", 2015),
-        helpText("Please type a year between 2004 and 2016"),
-        actionButton("update", "Update View")
-      ),
-      mainPanel(
-        plotOutput("plot")
-      )
-    )
+             sidebarLayout(
+               sidebarPanel(
+                 selectInput(
+                   "team",
+                   "Select a team:",
+                   nba2004 %>%
+                     distinct(tm, .keep_all = TRUE) %>%
+                     rename(Team = tm) %>%
+                     select(Team),
+                   selected = "GSW"
+                 ),
+                 numericInput("year", "Enter a year:", 2015),
+                 helpText("Please type a year between 2004 and 2016"),
+                 actionButton("update", "Update View")
+               ),
+               mainPanel(plotOutput("plot.p3"))
+             ))
   )
-)
+
 shinyUI(ui)
